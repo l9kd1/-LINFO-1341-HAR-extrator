67373feb257c64b2ad8e794dbf5ebd5faf918547
lgrumiaux <loic.grumiaux@uclouvain.be>
2021-04-10 12:51:45 -0700

    Initial commit.

---
+++ README.md
@@ -0,0 +1,22 @@
+## [LINFO 1341] HAR data extraction script
+
+### Késako ?
+Ce petit script python va vous permettre de générer plein de `.csv` utiles pour l'analyse de site demandée pour le cours de **[LINFO1341] Computer networks**.
+
+Ce serait vachement cool qu'on participe tous à son amélioration.
+
+En bonus voici des petits liens bien utiles:
+
+https://www.tablesgenerator.com/latex_tables
+
+https://roymartinez.dev/2019-06-05-har-analysis/
+
+### Comment m'utiliser ?
+Super simple !
+1. `git clone `.
+2. Tu génères un fichier .har avec ton navigateur préféré et tu le déplaces dans le répertoire où se trouve `analyse.py`.
+3. Tu modifies les variables `logname` et `analysed_domains` dans le script `analyse.py` avec les valeurs correctes.
+4. `python analyse.py` et le tour est joué !
+
+### Requirements
+Tu risques de devoir installer le package `pandas`.
---
+++ analyse.py
@@ -0,0 +1,213 @@
+"""
+Written by Loic Grumiaux - 37871800
+April 2020
+
+This website helped a great deal:
+https://roymartinez.dev/2019-06-05-har-analysis/
+
+"""
+
+import json, os,re
+import pandas as pd
+
+directory = os.getcwd()+'/'
+
+# CHANGE PARAMETERS BELOW
+
+#  Le chemin vers le .har à partir du répertoire du script python
+logname = 'inginious.har'
+#  Le nom de domaine du site
+analysed_domains = ['inginious.info.ucl.ac.be']
+
+with open(directory+logname, 'r') as f:
+    har = json.loads(f.read())
+    f.close()
+
+def findHeader(req,headertype,headername,op = None):
+    value = "None"
+    if headertype == 'response' or headertype=='request':
+        for h in req[headertype]['headers']:
+            if op == 'in':
+                if headername in h['name']:
+                    value = h['value']
+                    break
+            else:
+                if headername == h['name']:
+                    value = h['value']
+                    break
+
+    if headertype == 'cdn-timing':
+        value = 0
+        for h in req['response']['headers']:
+            if op == 'eq':
+                if 'server-timing' in h['name']:
+                    if headername in h['value']:
+
+                        value = int(h['value'].split(';')[1].split('=')[1])
+                        break
+        if value is None:
+            return 0
+    return value
+
+colmms = ['url',
+        'host',
+        'host-type',
+        'httpVersion',
+        'method',
+        'status',
+        'port_used',
+        'ext',
+        'server',
+        'size_snt',
+        'size_rcv']
+dat_clean = pd.DataFrame(columns=colmms)
+for r in har['log']['entries']:
+    u = str(r['request']['url']).split('?')[0]
+    if(r['response']['status']==0):
+        continue
+    host = re.search('://(.+?)/', u, re.IGNORECASE).group(0).replace(':','').replace('/','')
+
+
+    cachekey = str(findHeader(r,'response','x-cache-key','eq'))
+    if not cachekey == 'None':
+        cachekey = cachekey.split('/')
+        cpcode = int(cachekey[3])
+        ttl = cachekey[4]
+        cdnCache = str(findHeader(r,'response','x-cache','eq')).split(' ')[0]
+        cdnCacheParent = str(findHeader(r,'response','x-cache-remote','eq')).split(' ')[0]
+        origin = str(findHeader(r,'response','x-cache-key','eq')).split('/')[5]
+    else:
+        cachekey = "None"
+        cpcode = "None"
+        ttl = "None"
+        cdnCache = "None"
+        cdnCacheParent = "None"
+        origin = "None"
+
+    ext = re.search(r'(\.[A-Za-z0-9]+$)', u, re.IGNORECASE)
+    if any(tld in host for tld in analysed_domains):
+        hostType = 'First Party'
+    else:
+        hostType = 'Third Party'
+
+    if ext is None:
+        ext = "None"
+    else:
+        ext = ext.group(0).replace('.','')
+
+    send_sz = findHeader(r,'request','Content-Length','eq')
+    if(send_sz== "None"):
+        send_sz=0
+
+    rcv_sz = findHeader(r,'response','Content-Length','eq')
+    if(rcv_sz== "None"):
+        rcv_sz=0
+
+    try:
+        new_row = {
+            'url':u,
+            'host':host,
+            'host-type':hostType,
+            'httpVersion':r['request']['httpVersion'],
+            'method':r['request']['method'],
+            'status':r['response']['status'],
+            'port_used':r.get('connexion','0'),
+            'extension':ext,
+            'server':str(findHeader(r,'response','server','eq')),
+            'size_snt':((float(send_sz)+(r['request']['headersSize'] or 0))/(1024)),
+            'size_rcv':((float(rcv_sz)+(r['response']['headersSize'] or 0))/(1024))
+            }
+
+        dat_clean = dat_clean.append(new_row,ignore_index=True)
+    except Exception as e:
+        print("Non standard http request encountered url:")
+        print(u)
+        print(str(e))
+        print()
+
+# Stats based on the aggregation of fields
+
+agg_based_stats = [
+    {
+        "groupby":['host','url'],
+        "sortby":['host',"Size received from host [KB]"],
+        "aggfield":'size_rcv',
+        "csv_name":"input_traffic_all_urls",
+        "field":"Size received from host [KB]"
+    },
+    {
+        "groupby":['host','url'],
+        "sortby":['host',"Size sent to host [KB]"],
+        "aggfield":'size_snt',
+        "csv_name":"output_traffic_all_urls",
+        "field":"Size sent to host [KB]"
+    },
+    {
+        "groupby":['host'],
+        "sortby":["Size received from host [KB]"],
+        "aggfield":'size_rcv',
+        "csv_name":"input_traffic_host",
+        "field":"Size received from host [KB]"
+    },
+    {
+        "groupby":['host'],
+        "sortby":["Size sent to host [KB]"],
+        "aggfield":'size_snt',
+        "csv_name":"output_traffic_host",
+        "field":"Size sent to host [KB]"
+    },
+]
+
+# Track origin graph
+for param in agg_based_stats:
+    tmp = dat_clean
+    tmp = tmp.groupby(param["groupby"])[param["aggfield"]].agg('sum').reset_index(name=param["field"])
+    tmp = tmp.sort_values(by=param["sortby"],ascending=False)
+    tmp.to_csv(directory+'/'+param["csv_name"]+'.csv',index=False)
+    print("*"*10)
+    print(tmp)
+
+    del tmp
+
+
+# Stats based on the number of requests
+
+size_based_stats = [
+    {
+        "groupby":['port_used'],
+        "csv_name":"port_used"
+    },
+    {
+        "groupby":['httpVersion','host'],
+        "csv_name":"httpVersion_by_host"
+    },
+    {
+        "groupby":['httpVersion'],
+        "csv_name":"httpVersion"
+    },
+    {
+        "groupby":['host','extension'],
+        "csv_name":"type_data_host"
+    },
+    {
+        "groupby":['extension'],
+        "csv_name":"type_data"
+    },
+    {
+        "groupby":['method'],
+        "csv_name":"method_used"
+    }
+]
+
+field = "Requests"
+
+for param in size_based_stats:
+    tmp = dat_clean
+    tmp = tmp.groupby(param["groupby"]).size().reset_index(name=field)
+
+    tmp = tmp.sort_values(by=[field],ascending=False)
+    tmp.to_csv(directory+'/'+param["csv_name"]+'.csv',index=False)
+    print("*"*10)
+    print(tmp)
+
+    del tmp
